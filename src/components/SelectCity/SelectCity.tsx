5ecc4553dce930a48c1a5800af8f1403f3c4e709
NataliaK96 <ng_komarova@mail.ru>
2021-05-22 19:28:20 +0400

    feat: add select city

---
+++ src/components/SelectCity/SelectCity.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './SelectCity.module.scss';
+import { TPoint } from 'types';
+
+type Props = {
+  cities: TPoint[];
+  onSelect: (id: number) => void;
+};
+
+export const SelectCity: React.FC<Props> = ({ cities, onSelect }) => {
+  return (
+    <div className={style.select}>
+      <select onChange={(e) => onSelect(Number(e.target.value))}>
+        {cities.map((c) => (
+          <option value={c.id} key={c.id}>
+            {c.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
